71b7d78713ab97be12dc5abe34e5e1503b9a702f
1727kunal <kunalwaghamare1163@gmail.com>
2026-02-12 13:10:18 +0530

    base project update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = { "src/test/resources/Features/Login.feature" }, 
+		features = { "src/test/resources/Features" }, 
 		glue = { "stepdefinitions","hooks" }, 
 		monochrome = true, 
 		dryRun = false, 
